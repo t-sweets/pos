5ef69eb831028218da048ba33cb07860dc191aec
Yu Ishiguro <yuuis.egr@gmail.com>
2019-01-23 18:58:18 +0900

    add routing for device

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
 
-  scope :api do
+  namespace :api do
     scope :v1 do
+      mount_devise_token_auth_for 'Admin', at: 'auth'
       resources :products, only: [:index]
       resources :purchases, only: [:index, :create]
       resources :payment_methods, only: [:index]
       post "purchases/check" => "purchases#check"
-      post "login" => "admins#login"
     end
   end
 end
