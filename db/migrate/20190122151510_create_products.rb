a863ff8c527c898dc9aab8f4c91cae3d73cf9cd8
Yu Ishiguro <yuuis.egr@gmail.com>
2019-01-25 07:21:03 +0900

    fix default value of product display

--- db/migrate/20190122151510_create_products.rb
+++ db/migrate/20190122151510_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :image_path, null: false
       t.integer :cost
       t.integer :stock
-      t.boolean :display, null: true, :default => false
+      t.boolean :display, null: true, :default => true
       t.timestamps
     end
   end
