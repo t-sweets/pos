f098d803cbadcafec75da1dad0536f24e7a55dea
Yu Ishiguro <yuuis.egr@gmail.com>
2019-01-25 07:35:43 +0900

    add routing and method

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'Admin', at: 'auth'
-      resources :products, only: [:index]
+      resources :products, only: [:index, :update]
       resources :purchases, only: [:index, :create]
       resources :payment_methods, only: [:index]
       post "purchases/check" => "purchases#check"
