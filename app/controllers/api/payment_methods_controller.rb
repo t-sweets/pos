f9227bf6a223e13a21f769a73ad13d1e59dd0ca2
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-08 00:07:35 +0900

    set config to cashing off

--- app/controllers/api/payment_methods_controller.rb
+++ app/controllers/api/payment_methods_controller.rb
@@ -4,6 +4,7 @@ class Api::PaymentMethodsController < ApplicationController
   before_action :authenticate_admin_or_pos
 
   def index
+    expires_now
     @payment_methods = PaymentMethod.all
     render json: @payment_methods
   end
