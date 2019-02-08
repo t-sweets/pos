9b041f0a8f5078d21f9c4494f7f0fb8d0711884a
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-08 10:38:05 +0900

    fix auth action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,38 +6,38 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   def authenticate_admin_or_pos
-    return if user_signed_in? && (current_user.admin? || current_user.pos?)
+    return if user_signed_in? && (current_user.authority_id == 1 || current_user.authority_id == 2)
 
     render json: {
       success: false,
-      errors: ['purmission denied.']
+      errors: ['permission denied.']
     }, status: 401
   end
 
   def authenticate_admin
-    return if user_signed_in? && current_user.admin?
+    return if user_signed_in? && current_user.authority_id == 1
 
     render json: {
       success: false,
-      errors: ['purmission denied.']
+      errors: ['permission denied.']
     }, status: 401
   end
 
   def authenticate_admin_and_login_user
-    return if user_signed_in? && (current_user.admin? || current_user.email == params[:email])
+    return if user_signed_in? && (current_user.authority_id == 1 || current_user.email == params[:email])
 
     render json: {
       success: false,
-      errors: ['purmission denied.']
+      errors: ['permission denied.']
     }, status: 401
   end
 
   def authenticate_pos
-    return if user_signed_in? && current_user.pos?
+    return if user_signed_in? && current_user.authority_id == 2
 
     render json: {
       success: false,
-      errors: ['purmission denied.']
+      errors: ['permission denied.']
     }, status: 401
   end
 end
