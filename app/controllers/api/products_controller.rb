9926cbbfb9cac35662b8ca1a55a7e7902c63e42b
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-11 15:50:29 +0900

    add create method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -9,9 +9,10 @@ class Api::ProductsController < ApplicationController
   def create
     @product = Product.new(create_params)
     if @product.save
+      image_from_base64(params[:image]) if params[:image]
       render json: { success: true, product: @product }, status: :ok
     else
-      render json: { success: false, errors: [@purchase.errors] }, status: :unprocessable_entity
+      render json: { success: false, errors: [@product.errors] }, status: :unprocessable_entity
     end
   end
 
@@ -20,7 +21,7 @@ class Api::ProductsController < ApplicationController
     if @product&.update(update_params)
       render json: { success: true, product: @product }, status: :ok
     else
-      render json: { success: false, errors: [@purchase.errors] }, status: :unprocessable_entity
+      render json: { success: false, errors: [@product.errors] }, status: :unprocessable_entity
     end
   end
 
@@ -32,5 +33,14 @@ class Api::ProductsController < ApplicationController
 
   def update_params
     params.require(:product).permit(:name, :price, :stock, :display, :cost, :image_path)
+  end
+
+  def image_from_base64(b64)
+    bin = Base64.decode64(b64)
+    file = Tempfile.new('img')
+    file.binmode
+    file << bin
+    file.rewind
+    File.binwrite("public/product_images/#{@product.image_path}", file.read) # TODO: temporary storage
   end
 end
