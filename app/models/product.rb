6afa69bf9d19fd747021fa00168b02af72323da9
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-11 16:26:53 +0900

    fix conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,9 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :image_path, presence: true
   validates :display, inclusion: { in: [true, false] }
+  validates :notification, inclusion: { in: [true, false] }
 
   has_many :purchase_items, dependent: :restrict_with_error
-  has_many :purchass, through: :purchase_items
+  has_many :purchases, through: :purchase_items
   scope :active_all, -> { where(display: true) }
 end
--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -12,5 +12,14 @@ class PurchaseItem < ApplicationRecord
   def allocate_stock
     product = Product.find(product_id)
     product.update(stock: product.stock - quantity) if product.stock
+    nofify_slack if product.stock < product.notification.stock
+  end
+
+  private
+
+  def nofify_slack
+    # attachments = ''
+    text = product.name + 'が残り' + product.stock.to_s + 'です！ \n急いで買い出しに行きましょう！'
+    Slack.chat_postMessage(text: text, username: 'pos', channel: '\#random', icon_url: 'https://i.imgur.com/2aIp3nS.png')
   end
 end
