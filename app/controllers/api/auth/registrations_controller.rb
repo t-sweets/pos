c07f792d0b9de2cc593eed4efef6635f91c3c2a0
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-16 14:38:23 +0900

    fix user-update

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -6,6 +6,15 @@ module Api
       before_action :authenticate_admin, only: [:create]
       before_action :authenticate_admin_and_login_user, only: [:update]
 
+      def update
+        @user = User.find(params[:id])
+        if @user.update(account_update_params)
+          render json: { success: true, product: @user }, status: :ok
+        else
+          render json: { success: false, errors: [@user.errors] }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def sign_up_params
