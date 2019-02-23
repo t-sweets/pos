78600a6d7e9304c09b0ca0dacd0a7ca64869bc56
Yu Ishiguro <yuuis.egr@gmail.com>
2019-02-24 00:40:08 +0900

    add charge endpoint

---
+++ app/controllers/api/charges_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Api::ChargesController < ApplicationController
+  before_action :authenticate_admin_or_pos, only: [:create]
+
+  def create
+    @charge = Charge.new(create_params)
+    if @charge.save
+      log_audit(@charge, __method__)
+      render json: { success: true, purchase: @charge }, status: :ok
+    else
+      render json: { success: true, purchase: @charge.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:charge).permit(:payment_method_id, :amount)
+  end
+
+  def log_audit(model, operation)
+    AuditLog.create(model: 'charge', model_id: model.id, operation: operation, operator: current_user.id)
+  end
+end
