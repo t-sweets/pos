2356d36ab40e4335576a56c1a1116e6ee6490992
Yu Ishiguro <yuuis.egr@gmail.com>
2019-03-18 00:46:18 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,41 @@
 # frozen_string_literal: true
 
 # products
-Product.create(name: 'カップラーメン', price: 130, stock: 5, notification: true, notification_stock: 3, image_uuid: '2819b019-46e8-4f1d-9f83-4d248faf066f', image_path: '/product_images/2819b019-46e8-4f1d-9f83-4d248faf066f.png')
-Product.create(name: 'アイス', price: 100, stock: 3, notification: false, image_uuid: '94f5f6df-2ae9-496c-ac7d-18a9f267fd09', image_path: '/product_images/94f5f6df-2ae9-496c-ac7d-18a9f267fd09.png')
+Product.create(name: 'カップラーメン', price: 130, stock: 5, notification: true, notification_stock: 3, image_uuid: 'eba953f6-decf-453b-b6ec-fb2c283fc851', image_path: '/product_images/2819b019-46e8-4f1d-9f83-4d248faf066f.png')
+Product.create(name: 'アイス', price: 100, stock: 3, notification: false, image_uuid: 'eba953f6-decf-453b-b6ec-fb2c283fc851', image_path: '/product_images/94f5f6df-2ae9-496c-ac7d-18a9f267fd09.png')
 
 # payment method
 PaymentMethod.create(name: 'cash', addable: false, uuid: 'F5EF99BC-0FF0-4CA4-805D-5045E12B90CF')
 PaymentMethod.create(name: 't-pay', addable: true, uuid: '2ADEA824-0027-41B5-B243-10F2D24FDD4B')
 
 # purchases
-Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5')
-Purchase.create(payment_method_id: 2, payment_uuid: 'FF6577A6-10E1-425D-B09A-3BA1B8BBCCD3')
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5') # 1
+Purchase.create(payment_method_id: 2, payment_uuid: 'FF6577A6-10E1-425D-B09A-3BA1B8BBCCD3') # 2
 
 # purchase items
 PurchaseItem.create(purchase_id: 1, quantity: 2, product_id: 1, price: 100)
 PurchaseItem.create(purchase_id: 2, quantity: 1, product_id: 2, price: 100)
+
+# test purchases
+
+# 2018
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5', created_at: DateTime.new(2018, 2, 3.5)) # 3
+PurchaseItem.create(purchase_id: 3, quantity: 20_182, product_id: 1, price: 100, created_at: DateTime.new(2018, 2, 3.5))
+
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5', created_at: DateTime.new(2018, 6, 3.5)) # 4
+PurchaseItem.create(purchase_id: 4, quantity: 20_186, product_id: 2, price: 100, created_at: DateTime.new(2018, 6, 3.5))
+
+# 3
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5', created_at: DateTime.new(2018, 3, 3.5)) # 5
+PurchaseItem.create(purchase_id: 5, quantity: 20_183, product_id: 1, price: 100, created_at: DateTime.new(2018, 3, 3.5))
+PurchaseItem.create(purchase_id: 5, quantity: 20_183, product_id: 2, price: 100, created_at: DateTime.new(2018, 3, 3.5))
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5', created_at: DateTime.new(2019, 3, 3.5)) # 6
+PurchaseItem.create(purchase_id: 6, quantity: 20_193, product_id: 1, price: 100, created_at: DateTime.new(2019, 3, 3.5))
+PurchaseItem.create(purchase_id: 6, quantity: 20_193, product_id: 2, price: 100, created_at: DateTime.new(2019, 3, 3.5))
+
+Purchase.create(payment_method_id: 1, payment_uuid: '0C6B213E-1517-4B94-8F50-43F3DD77DBB5', created_at: DateTime.new(2010, 3, 3.5)) # 7
+PurchaseItem.create(purchase_id: 7, quantity: 2010, product_id: 1, price: 100, created_at: DateTime.new(2010, 3, 3.5))
+PurchaseItem.create(purchase_id: 7, quantity: 2010, product_id: 2, price: 100, created_at: DateTime.new(2010, 3, 3.5))
 
 # aurhorities
 Authority.create(name: 'admin')
