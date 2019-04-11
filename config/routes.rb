2f5d7fd06192d3be90d830e3d570689da7ee4027
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-12 01:12:22 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
       }
       post 'purchases/check' => 'purchases#check'
       get 'purchases/sales' => 'purchases#aggregate'
-      get 'balances' => 'balances#show'
-      post 'balances' => 'balances#init'
-      put 'balances' => 'balances#check'
+      get 'registers/balances' => 'registers#show'
+      post 'registers/balances' => 'registers#init'
+      put 'registers/check' => 'registers#check'
       post 'products/:id/stock' => 'products#add_stock'
       post 'products/:id/price' => 'products#increase_price'
       get 'products/jan/:code' => 'products#find_by_jan'
