02a24e44b4b1fce5e795388ca380b57b77437a1d
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-24 19:41:03 +0900

    fix auth

--- app/controllers/api/authorities_controller.rb
+++ app/controllers/api/authorities_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::AuthoritiesController < ApplicationController
-  before_action :authenticate_admin_or_pos, only: [:index]
-
   def index
     @authorities = Authority.all
     render json: @authorities
