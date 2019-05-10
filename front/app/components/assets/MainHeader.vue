ba1f33b1a34e3cbb893d34f6272c074e4e05309f
sho <sskmy1024.y@gmail.com>
2019-05-10 16:33:15 +0900

    add header menu

--- front/app/components/assets/MainHeader.vue
+++ front/app/components/assets/MainHeader.vue
@@ -13,10 +13,37 @@
       v-if="auth['access-token']"
     >
       <el-submenu index="2">
-        <template slot="title">ユーザー情報</template>
+        <template slot="title">{{ `ユーザー： ${user.name}` }}</template>
+        <el-menu-item index="2-1" @click="isConfirmDialog = true">パスワードの変更</el-menu-item>
         <el-menu-item index="2-5" @click="confirmLogout">ログアウト</el-menu-item>
       </el-submenu>
     </el-menu>
+
+    <el-dialog title="更新内容の確認" :visible.sync="isConfirmDialog" :append-to-body="true">
+      <el-form
+        ref="form"
+        :model="changePassForm"
+        label-width="150px"
+        :label-position="labelPosition"
+        size="small"
+        :rules="rules"
+        @submit.native.prevent="update"
+      >
+        <el-form-item label="Name" prop="name">
+          <p class="form-text">{{ user.name }}</p>
+        </el-form-item>
+        <el-form-item label="Password" prop="password" required>
+          <el-input
+            placeholder="Please input new password"
+            v-model="changePassForm.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" native-type="submit">変更</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -27,7 +54,25 @@ export default {
   data() {
     return {
       activeIndex: "1",
-      activeIndex2: "1"
+
+      isConfirmDialog: false,
+      changePassForm: {
+        password: ""
+      },
+      rules: {
+        password: [
+          {
+            required: true,
+            message: "空のパスワードは登録できません",
+            trigger: "blur"
+          },
+          {
+            min: 8,
+            message: "パスワードの長さは8文字以上にしてください",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -42,12 +87,47 @@ export default {
         this.$router.push("/admin/login/");
       });
     },
-    ...mapActions(["logout"])
+    async update() {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.$confirm("パスワードを変更します。よろしいですか？", "Warning", {
+            confirmButtonText: "OK",
+            cancelButtonText: "Cancel",
+            type: "warning"
+          }).then(async () => {
+            if (
+              await this.updateUser({
+                id: this.user.id,
+                password: this.changePassForm.password
+              })
+            ) {
+              this.isConfirmDialog = false;
+              this.$notify({
+                title: "Success",
+                message: "パスワードを変更しました。",
+                type: "success"
+              });
+            } else {
+              this.$alert("パスワードを変更できませんでした", "Error", {
+                confirmButtonText: "OK",
+                type: "success"
+              });
+            }
+          });
+        }
+      });
+    },
+    ...mapActions(["logout"]),
+    ...mapActions("users-manager", ["updateUser"])
   },
   computed: {
+    labelPosition() {
+      return window.matchMedia("(max-width:1024px)").matches ? "top" : "left";
+    },
     deviceScale() {
       return window.innerWidth;
     },
+    ...mapState(["user"]),
     ...mapState(["auth"])
   }
 };
